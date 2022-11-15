c168acdb4263cbeb784fedfb13a8190d876c0ec2
TheSensif <sergisimon7@gmail.com>
2022-11-15 20:36:27 +0100

    feat:Add encripted password sqlite

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,11 +1,18 @@
 package database;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.File;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Database {
 
-    public static void startDatabase() {
+    public static void startDatabase() throws SQLException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         String filePath = System.getProperty("user.dir") + "/src/database/database.db";
         // Conectar
 
@@ -16,8 +23,10 @@ public class Database {
             UtilsSQLite.queryUpdate(conn,"CREATE TABLE IF NOT EXISTS users (" +
                     "name varchar(15) PRIMARY KEY, password varchar(15));");
 
-            UtilsSQLite.queryUpdate(conn, "INSERT INTO users (nom, password) VALUES (\"admin\",\"1234\");");
+            UtilsSQLite.queryUpdate(conn, "INSERT INTO users (name, password) VALUES (\"admin\",\"" + UtilsSQLite.encrypt("1234") + "\");");
 
+            System.out.println(UtilsSQLite.querySelect(conn, "SELECT * FROM users;").getString(2));
+            System.out.println(UtilsSQLite.decrypt(UtilsSQLite.querySelect(conn, "SELECT password FROM users;").getString(1)));
             UtilsSQLite.disconnect(conn);
         }
 
--- src/database/UtilsSQLite.java
+++ src/database/UtilsSQLite.java
@@ -1,4 +1,10 @@
 package database;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.DatabaseMetaData;
@@ -6,8 +12,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Base64;
 
 public class UtilsSQLite {
+    public UtilsSQLite() throws NoSuchAlgorithmException {
+    }
+
+    //private static final String UNICODE_FORMAT = "UTF-8";
+
+
+
 
     public static Connection connect (String filePath) {
         Connection conn = null;
@@ -32,6 +46,59 @@ public class UtilsSQLite {
                 System.out.println("DDBB SQLite desconnectada");
             }
         } catch (SQLException ex) { System.out.println(ex.getMessage()); }
+    }
+
+    static SecretKey key;
+
+    static {
+        try {
+            key = generateKey(128);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static IvParameterSpec iv = generateIv();
+
+    public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(n);
+        SecretKey key = keyGenerator.generateKey();
+        return key;
+    }
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    public static String encrypt(String input) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        String algorithm = "AES/CBC/PKCS5Padding";
+        Cipher cipher = Cipher.getInstance(algorithm);
+
+
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        byte[] cipherText = cipher.doFinal(input.getBytes());
+        return Base64.getEncoder()
+                .encodeToString(cipherText);
+    }
+
+    public static String decrypt(String cipherText) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        String algorithm = "AES/CBC/PKCS5Padding";
+        Cipher cipher = Cipher.getInstance(algorithm);
+
+
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        byte[] plainText = cipher.doFinal(Base64.getDecoder()
+                .decode(cipherText));
+        return new String(plainText);
     }
 
     public static ArrayList<String> listTables (Connection conn) {
