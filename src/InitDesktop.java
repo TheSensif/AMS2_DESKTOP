2b7b91cbafd38e902249a1fc9c3a23dbda3900c3
David Ibáñez <73888235+DavidIbanezMoya@users.noreply.github.com>
2022-11-15 10:57:20 +0100

    Generating components and other stuff
    Changed the output messages to English
    Implemented some comments into the code

--- src/InitDesktop.java
+++ src/InitDesktop.java
@@ -1,5 +1,3 @@
-import database.Database;
-import modules.DataModule;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -26,13 +24,15 @@ import java.awt.event.InputEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
-public class InitDesktop extends JFrame {
-	private static InitDesktop frame;
+public class test extends JFrame {
+	private static test frame;
 	private JPanel contentPane;
 	private JScrollPane scrollPane;
-	
+	private Boolean activated;
+
 	/**
 	 * Launch the application.
 	 */
@@ -40,9 +40,9 @@ public class InitDesktop extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Database.startDatabase();
+					//Database.startDatabase();
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-					frame = new InitDesktop();
+					frame = new test();
 					frame.setTitle("IETI Industry");
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -55,7 +55,7 @@ public class InitDesktop extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public InitDesktop() throws IOException {
+	public test() throws IOException {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -63,11 +63,11 @@ public class InitDesktop extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu mnArchivo = new JMenu("Archivo");
+		JMenu mnArchivo = new JMenu("File");
 		menuBar.add(mnArchivo);
 		
-		JMenuItem mntmCargarConfiguracion = new JMenuItem("Cargar Configuracion");
-		// Cargar icono
+		JMenuItem mntmCargarConfiguracion = new JMenuItem("Charge configuration");
+		//Charging the icon
 		BufferedImage bi = ImageIO.read(new File(System.getProperty("user.dir") + "/src/images/load.png"));
 		Image icon = bi.getScaledInstance(16,16,Image.SCALE_SMOOTH);
 		mntmCargarConfiguracion.setIcon(new ImageIcon(icon));
@@ -80,38 +80,22 @@ public class InitDesktop extends JFrame {
 		mnArchivo.add(mntmCargarConfiguracion);
 		
 		
-		JMenu mnVisualizacion = new JMenu("Visualizacion");
+		JMenu mnVisualizacion = new JMenu("Visualization");
 		menuBar.add(mnVisualizacion);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		contentPane.setLayout(new GridLayout(0, 4));
 		setContentPane(contentPane);
 
-		scrollPane = new JScrollPane();
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-				gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-								.addContainerGap()
-								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 410, GroupLayout.PREFERRED_SIZE)
-								.addContainerGap(24, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-				gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 231, GroupLayout.PREFERRED_SIZE)
-								.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		contentPane.setLayout(gl_contentPane);
-		
 		
 		
 	}
 	
 	private void openFile() { // Open ChoserFile for .xml
 		HashMap<String,DataModule> dm = new HashMap<>();
+		//Generating a filter for the file
 		JFileChooser jf = new JFileChooser(System.getProperty("user.dir"));
-		FileNameExtensionFilter filtro = new FileNameExtensionFilter("Archivos XML","xml");
+		FileNameExtensionFilter filtro = new FileNameExtensionFilter("XML Files","xml");
 		jf.setFileFilter(filtro);
 		jf.showOpenDialog(this);
 		File archivo = jf.getSelectedFile();
@@ -119,6 +103,7 @@ public class InitDesktop extends JFrame {
 		if (archivo != null) {
 			String name = archivo.getName();
 			String [] part = name.split("\\.");
+			//If the file is filtered we will open it
 			if (Objects.equals(part[1],"xml")){
 				try {
 					DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -126,6 +111,7 @@ public class InitDesktop extends JFrame {
 					db = dbf.newDocumentBuilder();
 					Document doc = db.parse(archivo);
 
+					//We will store this element on a Datamodule if it exists
 					NodeList switc = doc.getElementsByTagName("switch");
 					if ( switc.getLength() != 0) {
 						for (int i = 0; i < switc.getLength(); i++) {
@@ -142,8 +128,8 @@ public class InitDesktop extends JFrame {
 						System.out.println(dm);
 					}
 
+					//We will store this element on a Datamodule if it exists
 					NodeList slider = doc.getElementsByTagName("slider");
-
 					if (slider.getLength() != 0) {
 						for (int i = 0; i < slider.getLength(); i++) {
 							Node node = slider.item(i);
@@ -157,8 +143,9 @@ public class InitDesktop extends JFrame {
 						System.out.println(dm);
 
 					}
+					
+					//We will store this element on a Datamodule if it exists
 					NodeList dropdown = doc.getElementsByTagName("dropdown");
-
 					if (dropdown.getLength() != 0) {
 						for (int i = 0; i < dropdown.getLength(); i++) {
 							Node node = dropdown.item(i);
@@ -180,8 +167,8 @@ public class InitDesktop extends JFrame {
 							System.out.println(dm);
 						}
 
+						//We will store this element on a Datamodule if it exists
 						NodeList sensor = doc.getElementsByTagName("sensor");
-
 						if (sensor.getLength() != 0) {
 							for (int i = 0; i < sensor.getLength(); i++) {
 								Node node = sensor.item(i);
@@ -197,16 +184,109 @@ public class InitDesktop extends JFrame {
 						}
 
 					}
-					System.out.println(dm.size());
+					
+					ArrayList<String> componentes = new ArrayList<>();
+				
+
+					//We will create an array with the length of the quantity of the components
+					//The array will be the quanity but with formated to string, to match the information that the XML provides
 					for (int i = 0; i < dm.size(); i++) {
-						switch (dm.get(i).getEtiqueta()) {
+						componentes.add(String.valueOf(i));
+					}
+					
+					//Update the panel view
+					contentPane.revalidate();
+					contentPane.repaint();
+
+					
+					for (int i = 0; i < dm.size(); i++) {		
+						//We will look to get the component id with its tagName
+						//Depending on the tagname it will create a diferent component
+						switch (dm.get(componentes.get(i)).getEtiqueta()) {
 							case "switch":
+								scrollPane = new JScrollPane();
+								
+								JToggleButton tglbtn = new JToggleButton("");
+								//We will get the inital state specified on the xml
+								if ((dm.get(componentes.get(i)).getDefaul().equals("on"))) {
+									activated = true;
+									tglbtn.setText("Active");
+								} else {
+									activated = false;
+									tglbtn.setText("Not active");
+
+								}
+								
+								tglbtn.addActionListener(new ActionListener() {
+									
+									//Changing its state every time its pressed
+									public void actionPerformed(ActionEvent e) {
+										if (activated == true) {
+											tglbtn.setText("Not active");
+											tglbtn.disable();
+											activated = false;
+										}
+										else {
+											tglbtn.setText("Active");
+											tglbtn.enable();
+											activated = true;
+										}
+									}
+								});;
+								scrollPane.setViewportView(tglbtn);
+								contentPane.add(scrollPane);
+								
 								break;
 							case "slider":
+								JSlider jSlider = new JSlider();
+								scrollPane = new JScrollPane();
+
+								//Firstly we will get it as a string, then we will have to convert it into a double because it has a decimal, finally we will convert it into int
+								jSlider.setValue(Double.valueOf((dm.get(componentes.get(i)).getDefaul())).intValue());
+								scrollPane.setViewportView(jSlider);
+								contentPane.add(scrollPane);
+
 								break;
 							case "dropdown":
+								JComboBox comboBox = new JComboBox();
+								scrollPane = new JScrollPane();
+
+								//Creating an arraylist with the keys of the hashmap
+								ArrayList<String> dropdownKeys = new ArrayList<>();
+								for (Object key : dm.get(componentes.get(i)).getValue().keySet()) {
+								    String lKey = (String) key;
+								    dropdownKeys.add(lKey);
+								}
+								
+								//We will iterate a for over with the Id quantity
+								for (int j = 0; j < dropdownKeys.size(); j++) {
+									
+									//Checking if the iteration is the same of the deafult value
+									if (String.valueOf(dropdownKeys.get(j)).equals(String.valueOf(dm.get(componentes.get(i)).getDefaul()))) {
+										comboBox.addItem((dm.get(componentes.get(i)).getValue().get(dropdownKeys.get(j))));
+										//If it is, we will set it as selected Item
+										comboBox.setSelectedItem((dm.get(componentes.get(i)).getValue().get(dropdownKeys.get(j))));
+									} else {
+										//If not instead we just add it at the Dropdown
+										comboBox.addItem((dm.get(componentes.get(i)).getValue().get(dropdownKeys.get(j))));
+									}
+									
+								}
+								
+								scrollPane.setViewportView(comboBox);
+								contentPane.add(scrollPane);
 								break;
 							case "sensor":
+								JTextArea textArea = new JTextArea();
+								scrollPane = new JScrollPane();
+								//The sensor will be a textArea with the specified units
+								textArea.setText(dm.get(componentes.get(i)).getUnits());
+								//Enabled at false to not let the user modify it
+								textArea.setEnabled(false);
+								
+								scrollPane.setViewportView(textArea);
+								contentPane.add(scrollPane);
+
 								break;
 						}
 					}
@@ -220,9 +300,9 @@ public class InitDesktop extends JFrame {
 					throw new RuntimeException(e);
 				}
 
-				System.out.println("Es un xml");
+				System.out.println("It is xml");
 			}else {
-				System.out.println("No es un xml");
+				System.out.println("It is not an xml");
 			}
 
 		}
